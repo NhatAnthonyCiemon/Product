de15afe0a7bafe933e3f05e21b92bb5b0557596f
TNMonster <nhatmonsterhack@gmail.com>
2024-11-03 15:16:52 +0700

    f

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -1,10 +1,17 @@
 const db = require("../../config/db");
 
 const Product = {
-    index: async (req, res) => {
-        //truy cập dữ liệu từ database
+    index: async (object) => {
+        //truy cập dữ liệu từ database(ORM)
+        db.query("SELECT * FROM products", (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(result);
+            }
+        });
     },
-    detail: async (req, res) => {
+    detail: async (object) => {
         //truy cập dữ liệu từ database
     },
 };
