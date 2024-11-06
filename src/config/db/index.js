7f10f433318a07a52f459c3e8f3b0b6362964ac4
TNMonster <nhatmonsterhack@gmail.com>
2024-11-06 21:01:36 +0700

    f

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,12 +1,17 @@
 const { Sequelize } = require("sequelize");
 const mysql2 = require("mysql2");
 
-const sequelize = new Sequelize("sql12741852", "sql12741852", "WYEkbSB41E", {
-    host: "sql12.freemysqlhosting.net",
-    dialectModule: mysql2, // Địa chỉ host của MySQL
-    dialect: "mysql", // Chọn dialect cho MySQL
-    logging: false, // Tắt logging SQL (tuỳ chọn)
-});
+const sequelize = new Sequelize(
+    "freedb_productBTV",
+    "freedb_productBTVN",
+    "w5RXf#XsmdQ#XC4",
+    {
+        host: "sql.freedb.tech",
+        dialectModule: mysql2, // Địa chỉ host của MySQL
+        dialect: "mysql", // Chọn dialect cho MySQL
+        logging: false, // Tắt logging SQL (tuỳ chọn)
+    }
+);
 try {
     sequelize
         .authenticate()
