87f3582ff2d39b656aa768d547cc384bff7a84d0
TNMonster <nhatmonsterhack@gmail.com>
2024-11-06 20:02:48 +0700

    f

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,23 +1,28 @@
 const { Sequelize } = require("sequelize");
+const mysql2 = require("mysql2");
 
 const sequelize = new Sequelize(
     "freedb_productBTV",
     "freedb_productBTVN",
     "w5RXf#XsmdQ#XC4",
     {
-        host: "sql.freedb.tech", // Địa chỉ host của MySQL
+        host: "sql.freedb.tech",
+        dialectModule: mysql2, // Địa chỉ host của MySQL
         dialect: "mysql", // Chọn dialect cho MySQL
         logging: false, // Tắt logging SQL (tuỳ chọn)
     }
 );
-
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log("Connection has been established successfully.");
-    })
-    .catch((error) => {
-        console.error("Unable to connect to the database:", error);
-    });
+try {
+    sequelize
+        .authenticate()
+        .then(() => {
+            console.log("Connection has been established successfully.");
+        })
+        .catch((error) => {
+            console.error("Unable to connect to the database:", error);
+        });
+} catch (error) {
+    console.log(error);
+}
 
 module.exports = sequelize;
